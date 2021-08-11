ad36328bdf2a8ab38c0a03ee405cc1d6a24bd600
Silviu Barbu <silviubarbu@bitbucket.org>
2021-08-11 11:43:39 +0300

    added comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { PricingComponent } from './components/pricing/pricing.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HowitworksComponent } from './components/howitworks/howitworks.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    PricingComponent,
+    AboutComponent,
+    ContactComponent,
+    HowitworksComponent
   ],
   imports: [
     BrowserModule,
